db1fa1abff63dd4e8fd2cd985dd276b6b8aafafe
Dominic Felicelli <dofedff@student.oaisd.org>
2023-03-16 13:40:52 -0400

    Final commit for Snow Board

--- Assets/CrashDetector.cs
+++ Assets/CrashDetector.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadDelay = .5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool once = true;
 
     void OnCollisionEnter2D(Collision2D other) 
     {
-        crashEffect.Play();
+        FindObjectOfType<PlayerController>().DisableControls();
+        if (once)
+        {
+            crashEffect.Play();
+            once = false;
+        }
+        GetComponent<AudioSource>().Play();
         Invoke("ReloadScene", reloadDelay);
+        once = true;
     }
-    void ReloadScene()
+    public void ReloadScene()
     {
         SceneManager.LoadScene(0);
     }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2D;
-    [SerializeField] float torqueAmount = 1f;
+    SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
 
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] ParticleSystem dustEffect;
+    
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -23,5 +52,17 @@ public class PlayerController : MonoBehaviour
         {
             rb2D.AddTorque(-torqueAmount);
         }
+    }
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        dustEffect.Play();
+    }
+    void OnCollisionExit2D(Collision2D other) 
+    {
+        dustEffect.Stop();
+    }
+    public void DisableControls() 
+    {
+        canMove = false;
     }
 }
